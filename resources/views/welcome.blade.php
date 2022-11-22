99eecf53ae9c6375018bd48d465ff1419f9ccbcf
HninHtet-99 <hninhtet1113@gmail.com>
2022-11-22 13:16:09 +0630

    blade layout,yield and section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog System</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <style>
-        .bg-warning{
-            background-color: yellow;
-        }
-    </style>
-</head>
-<body>
-    @if(true)
-       <p>Hello World</p>
-    
-    @endif
-    @unless(false)
-        <h1>This is reverse of if condition..</h1>
-    @endunless
-   @foreach ($blogs as $blog)
-        <div class="{{$loop->even?'bg-warning':''}}">
-            <h2><a href="blogs/{{$blog->slug}}">{{$blog-> title}}</a></h2>        
-            <p>published at :: {{$blog->date}}</p>
-            <p>{{$blog-> intro}}</p>
-        </div>
-    @endforeach
-</body>
-</html>
+@extends('layout')
+
+@section('title')
+<title>All Blogs</title>
+@endsection
+
+@section('content')
+
+@foreach ($blogs as $blog)
+    <h2><a href="blogs/{{$blog->slug}}">{{$blog-> title}}</a></h2>        
+    <p>published at :: {{$blog->date}}</p>
+    <p>{{$blog-> intro}}</p>
+@endforeach
+
+@endsection
