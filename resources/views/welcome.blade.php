a71962d66e52c6f5a34c35d39f548e4f68122eb5
HninHtet-99 <hninhtet1113@gmail.com>
2022-12-14 15:48:03 +0630

    belongs to relationship

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
         <title>All Blogs</title>
     </x-slot>
     @foreach ($blogs as $blog)
-        <h2><a href="blogs/{{$blog->id}}">{{$blog-> title}}</a></h2>        
+        <h2><a href="blogs/{{$blog->slug}}">{{$blog-> title}}</a></h2>
+        <h4>category :: {{$blog -> category->name}}</h4>        
         <p>published at :: {{$blog->created_at->diffForHumans()}}</p>
         <p>{{$blog-> intro}}</p>
     @endforeach
