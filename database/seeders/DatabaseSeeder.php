5cba4df1f1e4e9aeb3faf07bae4089e499d6d2b0
HninHtet-99 <hninhtet1113@gmail.com>
2022-12-20 20:24:16 +0630

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $frontend = \App\Models\Category::create([
+                        'name' => 'Front-end',
+                        'slug' => 'front-end',
+                    ]);
+        $backend = \App\Models\Category::create([
+            'name' => 'Back-end',
+            'slug' => 'back-end',
+        ]);
+
+        \App\Models\Blog::create([
+            'title'=>'This is frontend post',
+            'slug'=>'front-end-post',
+            'intro'=>'This is intro',
+            'body'=> "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            'category_id'=> $frontend->id
+
+        ]);
+        \App\Models\Blog::create([
+            'title'=>'This is backend post',
+            'slug'=>'front-end-post',
+            'intro'=>'This is intro',
+            'body'=> "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            'category_id'=> $backend->id
+
+        ]);
     }
 }
