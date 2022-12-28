93f4da2471bda5a4457825b9b13434e3b1774d80
HninHtet-99 <hninhtet1113@gmail.com>
2022-12-28 20:45:30 +0630

    inRandomOrder

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,8 +1,25 @@
 <x-layout>
-    <x-slot name="title">
-        <title>{{$blog->title}}</title>
-    </x-slot>
-    <h2>{{$blog->title}}</h2>
-    <p>{!! $blog->body !!}</p>
-    <a href="/">go back</a>
+
+    <!-- single blog section -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mx-auto text-center">
+          <img
+            src="https://creativecoder.s3.ap-southeast-1.amazonaws.com/blogs/GOLwpsybfhxH0DW8O6tRvpm4jCR6MZvDtGOFgjq0.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <h3 class="my-3">{{$blog->title}}</h3>
+          <h5 class="badge bg-primary">{{$blog->category->name}}</h5>
+          <h5>Author :: {{$blog->author->name}}</h5>
+          <p>{{$blog->created_at->diffForHumans()}}</p>
+          <p class="lh-md">
+            {{$blog->body}}
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <x-subscribe/>
+    <x-blog_you_may_like :randomBlogs="$randomBlogs"/>
 </x-layout>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,10 @@
-<nav>
-    <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Content</a></li>
-    </ul>
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="./index.html">Creative Coder</a>
+        <div class="d-flex">
+            <a href="#home" class="nav-link">Home</a>
+            <a href="#blogs" class="nav-link">Blogs</a>
+            <a href="#subscribe" class="nav-link">Subscribe</a>
+        </div>
+    </div>
 </nav>
